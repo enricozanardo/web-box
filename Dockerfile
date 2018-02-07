1ec417bf211e2fa6e76d132876d65df0124379d2
Enrico Zanardo <ezanardo@onezerobinary.com>
2018-02-07 18:56:45 +0100

    add database docker settings

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,9 @@ ENV SRC_DIR=/go/src/github.com/onezerobinary/web-box
 # Add the source code:
 ADD . $SRC_DIR
 # Build it:
+RUN go get -v -u github.com/Masterminds/glide
+RUN export PATH=$SRC_DIR/bin:$PATH
+RUN cd $SRC_DIR; glide install
+
 RUN cd $SRC_DIR; go build -o myapp; cp myapp /app/
 ENTRYPOINT ["./myapp"]
--- app.go
+++ app.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
-
-"github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Hello)
 	http.Handle("/", r)
-	fmt.Println("Starting up on 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Starting up on 8800")
+	log.Fatal(http.ListenAndServe(":8800", nil))
 }
 
 func Hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
+	fmt.Fprintln(w, "Hello Enrico!")
 }
