8600dafb6b40b72539493b1eb56f82ee6aa2a75e
Enrico Zanardo <ezanardo@onezerobinary.com>
2018-03-07 10:17:07 +0100

    Create and Test all the grpc endpoints

--- mygprc/GRPCaccountClient.go
+++ mygprc/GRPCaccountClient.go
@@ -1,15 +1,14 @@
 package mygprc
 
-
 import (
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"
 	"github.com/goinggo/tracelog"
 	"os"
 	"crypto/sha1"
 	"encoding/hex"
 	"io"
 	pb_account "github.com/onezerobinary/db-box/proto/account"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -49,7 +48,7 @@ func GenerateToken(username string, password string) (token string){
 	return token
 }
 
-func CreateAccount(account *pb_account.Account) (accountResponse *pb_account.Response)  {
+func CreateAccount(account *pb_account.Account) (response *pb_account.Response)  {
 
 	conn := StartGRPCConnection()
 	defer StopGRPCConnection(conn)
@@ -92,7 +91,8 @@ func GetAccountByCredentials(credentials *pb_account.Credentials)(account *pb_ac
 }
 
 // Get an Account given the Token
-func GetAccountByToken(token *pb_account.Token)(pb_account.Account) {
+func GetAccountByToken(token *pb_account.Token)(account *pb_account.Account) {
+
 	conn := StartGRPCConnection()
 	defer StopGRPCConnection(conn)
 
@@ -107,42 +107,163 @@ func GetAccountByToken(token *pb_account.Token)(pb_account.Account) {
 
 	tracelog.Trace("GRPCaccountClient", "GetAccountByToken", "An account is retrieved")
 
-	return *account
+	return account
 }
 
 // Update an Account given the updated Account
-func UpdateAccount (account *pb_account.Account )(response pb_account.Response) {
-	return
+func UpdateAccount (account *pb_account.Account )(response *pb_account.Response) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+	// Search into the DB the user
+	client := pb_account.NewAccountServiceClient(conn)
+
+	resp, err := client.UpdateAccount(context.Background(), account)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "UpdateAccount", "Error: Account not updated")
+		os.Exit(1)
+	}
+
+	if resp.Code == 200 {
+		tracelog.Trace("GRPCaccountClient", "UpdateAccount", "Account is updated into DB")
+	} else {
+		tracelog.Trace("GRPCaccountClient", "UpdateAccount", "It was not possible to update the Account into DB")
+	}
+
+	return resp
 }
 
 // Delete an Account given the Token
-func DeleteAccount (token *pb_account.Token)(response pb_account.Response) {
-	return
+func DeleteAccount (token *pb_account.Token)(response *pb_account.Response) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+	// Search into the DB the user
+	client := pb_account.NewAccountServiceClient(conn)
+
+	resp, err := client.DeleteAccount(context.Background(), token)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "DeleteAccount", "Error: It is not possible to delete the account")
+		os.Exit(1)
+	}
+
+	if resp.Code == 200 {
+		tracelog.Trace("GRPCaccountClient", "DeleteAccount", "Account deleted from DB")
+	} else {
+		tracelog.Trace("GRPCaccountClient", "DeleteAccount", "It was not possible to delete the Account from the DB")
+	}
+
+	return resp
 }
 
 // Check if an email address is already used
-func CheckEmail (email *pb_account.Email)(response pb_account.Response) {
-	return
+func CheckEmail (email *pb_account.Email)(response *pb_account.Response) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+	// Search into the DB the user
+	client := pb_account.NewAccountServiceClient(conn)
+
+	resp, err := client.CheckEmail(context.Background(), email)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "CheckEmail", "Error: It is not possible to check the email")
+		os.Exit(1)
+	}
+
+	if resp.Code == 200 {
+		tracelog.Trace("GRPCaccountClient", "CheckEmail", "Email successfully checked")
+	} else {
+		tracelog.Trace("GRPCaccountClient", "CheckEmail", "It was not possible to check the given email into the DB")
+	}
+
+	return resp
 }
 
 // Get the Status of an account given the Token
-func GetAccountStatus (token *pb_account.Token) (status pb_account.Status) {
-	return
+func GetAccountStatus (token *pb_account.Token) (status *pb_account.Status) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+
+	client := pb_account.NewAccountServiceClient(conn)
+
+	status, err := client.GetAccountStatus(context.Background(), token)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "GetAccountStatus", "Error: It was not possible to retrieve the account status")
+		os.Exit(1)
+	}
+
+	tracelog.Trace("GRPCaccountClient", "GetAccountStatus", "Account status is retrieved")
+
+	return status
 }
 
 // Set the Status of an account given the Updated Status
-func SetAccountStatus (updateStatus *pb_account.UpdateStatus)(response pb_account.Response) {
-	return
+func SetAccountStatus (updateStatus *pb_account.UpdateStatus)(response *pb_account.Response) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+	// Search into the DB the user
+	client := pb_account.NewAccountServiceClient(conn)
+
+	resp, err := client.SetAccountStatus(context.Background(), updateStatus)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "SetAccountStatus", "Error: It is not possible to set the account status")
+		os.Exit(1)
+	}
+
+	if resp.Code == 200 {
+		tracelog.Trace("GRPCaccountClient", "SetAccountStatus", "Account status successfully updated")
+	} else {
+		tracelog.Trace("GRPCaccountClient", "SetAccountStatus", "It was not possible to update the account status into the DB")
+	}
+
+	return resp
 }
 
 // Get all the accounts based on a specific Status
-func GetAccountsByStatus (status *pb_account.Status)(accounts pb_account.Accounts ) {
-	return
+func GetAccountsByStatus (status *pb_account.Status)(accounts *pb_account.Accounts ) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+
+	client := pb_account.NewAccountServiceClient(conn)
+
+	accounts, err := client.GetAccountsByStatus(context.Background(), status)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "GetAccountsByStatus", "Error: It was not possible to retrieve the accounts")
+		os.Exit(1)
+	}
+
+	tracelog.Trace("GRPCaccountClient", "GetAccountsByStatus", "Accounts are successfully retrieved")
+
+	return accounts
 }
 
 //Get the account collection
-func GetAccounts (empty pb_account.Empty)(accounts pb_account.Accounts) {
-	return
+func GetAccounts (empty *pb_account.Empty)(accounts *pb_account.Accounts) {
+
+	conn := StartGRPCConnection()
+	defer StopGRPCConnection(conn)
+
+	client := pb_account.NewAccountServiceClient(conn)
+
+	accounts, err := client.GetAccounts(context.Background(), empty)
+
+	if err != nil {
+		tracelog.Errorf(err, "GRPCaccountClient", "GetAccounts", "Error: It was not possible to retrieve the accounts")
+		os.Exit(1)
+	}
+
+	tracelog.Trace("GRPCaccountClient", "GetAccounts", "Accounts are successfully retrieved")
+
+	return accounts
 }
 
 
--- mygprc/GRPCaccountClient_test.go
+++ mygprc/GRPCaccountClient_test.go
@@ -13,17 +13,20 @@ func TestCreateAccount(t *testing.T){
 
 	//fakeStatus := pb_account.Status{pb_account.Status_NOTSET}
 
-	username := "Zorro"
-	password := "Zirro"
+	username := "Zippi"
+	password := "Zirri"
 
 	faketoken := GenerateToken(username, password)
+
+	token := pb_account.Token{faketoken}
+	status := pb_account.Status{pb_account.Status_NOTSET}
 
 	fakeAccount := pb_account.Account{
 		faketoken,
 		username,
 		password,
-		nil,
-		nil,
+		&token,
+		&status,
 		"Account",
 		"2018-01-11",
 		"2028-01-10",
@@ -36,18 +39,17 @@ func TestCreateAccount(t *testing.T){
 	}
 }
 
-
 func TestGetAccountByToken(t *testing.T) {
 
 	tracelog.Start(tracelog.LevelTrace)
 	defer tracelog.Stop()
 
-	//username := "Zorro"
-	//password := "Zirro"
+	username := "Zippo"
+	password := "Zirro"
 
-	//faketoken := GenerateToken(username, password)
+	faketoken := GenerateToken(username, password)
 
-	token := pb_account.Token{"ae30c48577a9e11421ed5434259c3fd88a6c5587"}
+	token := pb_account.Token{faketoken}
 
 	account := GetAccountByToken(&token)
 
@@ -55,7 +57,6 @@ func TestGetAccountByToken(t *testing.T) {
 		t.Errorf("Error in retrieving the account")
 	}
 }
-
 
 func TestGetAccountByCredentials(t *testing.T) {
 
@@ -77,3 +78,140 @@ func TestGetAccountByCredentials(t *testing.T) {
 		t.Errorf("Error in retrieving the account")
 	}
 }
+
+func TestUpdateAccount(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	username := "Zippo"
+	password := "Zirro"
+
+	faketoken := GenerateToken(username, password)
+
+	token := pb_account.Token{faketoken}
+	status := pb_account.Status{pb_account.Status_ENABLED}
+
+	fakeAccount := pb_account.Account{
+		faketoken,
+		username,
+		password,
+		&token,
+		&status,
+		"Account",
+		"2018-01-11",
+		"2058-01-10",
+	}
+
+	response := UpdateAccount(&fakeAccount)
+
+	if response.Code != 200 {
+		t.Errorf("Error to update the account")
+	}
+}
+
+func TestDeleteAccount(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	username := "Tino"
+	password := "Zuccon"
+
+	faketoken := GenerateToken(username, password)
+
+	token := pb_account.Token{faketoken}
+
+	response := DeleteAccount(&token)
+
+	if response.Code != 200 {
+		t.Errorf("Error to delete the account")
+	}
+}
+
+func TestCheckEmail(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	fakeEmail := pb_account.Email{"Tano"}
+
+	response := CheckEmail(&fakeEmail)
+
+	if response.Token == nil {
+		tracelog.Warning("", "checkemail", "No email are found")
+	}
+
+	if response.Code != 200 {
+		t.Errorf("Error to check the email")
+	}
+}
+
+func TestGetAccountStatus(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	faketoken := pb_account.Token{"21718086c9234b00096229ce2697b8f1d2a7acaf"}
+
+	status := GetAccountStatus(&faketoken)
+
+	if status.Status == pb_account.Status_NOTSET {
+		t.Errorf("Error to retrieve the account status")
+	}
+
+}
+
+func TestSetAccountStatus(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	faketoken := pb_account.Token{"21718086c9234b00096229ce2697b8f1d2a7acaf"}
+	newStatus := pb_account.Status{pb_account.Status_SUSPENDED}
+
+	updateStatus := pb_account.UpdateStatus{&faketoken, &newStatus}
+
+	response := SetAccountStatus(&updateStatus)
+
+	if response.Code != 200 {
+		t.Errorf("Error to update the account status")
+	}
+}
+
+func TestGetAccountsByStatus(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	fakeStatus := pb_account.Status{pb_account.Status_SUSPENDED}
+
+	accounts :=GetAccountsByStatus(&fakeStatus)
+
+	if accounts == nil {
+		t.Errorf("Empty accounts with the given status are retreived")
+	}
+
+	if len(accounts.Accounts) == 0 {
+		t.Errorf("Zero accounts with the given status are retreived")
+	}
+}
+
+func TestGetAccounts(t *testing.T) {
+
+	tracelog.Start(tracelog.LevelTrace)
+	defer tracelog.Stop()
+
+	empty := pb_account.Empty{}
+
+	accounts := GetAccounts(&empty)
+
+	if accounts == nil {
+		t.Errorf("Empty accounts are retreived")
+	}
+
+	if len(accounts.Accounts) == 0 {
+		t.Errorf("Zero accounts are retreived")
+	}
+}
+
